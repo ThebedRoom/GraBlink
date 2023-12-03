fe14b2edfdaba148b32c978bac75218e47c0e544
ephing <ethangrantz201@gmail.com>
2023-12-03 13:28:42 -0600

    improved testing scripts

--- run_test.bash
+++ run_test.bash
@@ -10,8 +10,6 @@ TEST_DIR="$(realpath benchmarks/$1)"
 RUN_DIR="$(realpath run_tests/$1)"
 export GRABLINK="$(realpath ./target/release/grablink)"
 
-"$GRABLINK" --time --egraph "$TEST_DIR" > "$RUN_DIR/egraph.out" 2> "$RUN_DIR/egraph.err"
-"$GRABLINK" --time --vsa "$TEST_DIR" > "$RUN_DIR/vsa.out" 2> "$RUN_DIR/vsa.err"
-# "$GRABLINK" --time --enum "$TEST_DIR" > "$RUN_DIR/enum.out" 2> "$RUN_DIR/enum.err"
+"$GRABLINK" --time --$2 "$TEST_DIR" > "$RUN_DIR/$2.out" 2> "$RUN_DIR/$2.err"
 
-printf "${GREEN}[FINISHED]${NC} ${1}\n"
+printf "${GREEN}[FINISHED]${NC} ${1} ${2}\n"
--- run_tests.bash
+++ run_tests.bash
@@ -8,11 +8,25 @@ cd benchmarks
 ANY_FAIL=no
 
 for test in *; do
+        
         cd ..
-        "$RUN_TEST" "$test"
-        if [ $? -eq 1 ]; then
-            ANY_FAIL=yes
+        if ! [ -d $(realpath run_tests/$test) ]; then {
+            mkdir -p "run_tests/$test"
+        }
         fi
+        if ! [ -f $(realpath run_tests/$test/egraph.out) ]; then
+            "$RUN_TEST" "$test" "egraph"
+            if [ $? -eq 1 ]; then
+                ANY_FAIL=yes
+            fi
+        fi
+        # if ! [ -f "run_tests/$test/vsa.out" ]; then
+        #     "$RUN_TEST" "$test" "vsa"
+        #     if [ $? -eq 1 ]; then
+        #         ANY_FAIL=yes
+        #     fi
+        # fi
+        
         cd benchmarks
 done
 
